ea3fb252235a0308b88f87110ce97b2a86256050
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-01 15:34:12 +0800

    style changes
    added booking page

---
+++ Pages/Booking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace south_country_garden.Pages
+{
+    public class BookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
