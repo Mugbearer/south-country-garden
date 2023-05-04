6fc4c2eb10cc06e31d5fdb4572d60a2fd6432622
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-04 18:29:44 +0800

    food model updated

--- Model/food_arrangements.cs
+++ Model/food_arrangements.cs
@@ -5,5 +5,29 @@ namespace south_country_garden.Model
     public class food_arrangements
     {
         public int booking_id { get; set; }
+
+        [MaxLength(255)]
+        public string beef { get; set; }
+
+        [MaxLength(255)]
+        public string seafood { get; set; }
+
+        [MaxLength(255)]
+        public string pork { get; set; }
+
+        [MaxLength(255)]
+        public string chicken { get; set; }
+
+        [MaxLength(255)]
+        public string pasta { get; set; }
+
+        [MaxLength(255)]
+        public string vegetable { get; set; }
+
+        [MaxLength(255)]
+        public string rice { get; set; }
+
+        [MaxLength(255)]
+        public string dessert { get; set; }
     }
 }
