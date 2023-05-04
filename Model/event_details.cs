f484de8b28c6b555af1b2ffa5c30b5f81e03dcdc
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-04 18:45:11 +0800

    event details model updated

--- Model/event_details.cs
+++ Model/event_details.cs
@@ -5,5 +5,28 @@ namespace south_country_garden.Model
     public class event_details
     {
         public int booking_id { get; set; }
+
+        [MaxLength(50)]
+        public string event_title { get; set; }
+
+        [MaxLength(255)]
+        public string color_motif { get; set; }
+
+        [MaxLength(10)]
+        public string event_date { get; set; }
+
+        [MaxLength(50)]
+        public string church { get; set; }
+
+        [MaxLength(100)]
+        public string venue { get; set; }
+
+        [MaxLength(100)]
+        public string time_preparation { get; set; }
+
+        [Range(1,400)]
+        [Required]
+        public int pax_number { get; set; }
+        public string event_time { get; set; }
     }
 }
