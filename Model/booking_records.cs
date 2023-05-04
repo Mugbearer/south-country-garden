ad4fc973980d7ad65e30c0c948692251ce6aec94
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-04 15:44:44 +0800

    updated booking records model

---
+++ Model/booking_records.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace south_country_garden.Model
+{
+    public class booking_records
+    {
+        [Key]
+        public int booking_id { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public int contact_number { get; set; }
+
+        public string address { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        public string event_title { get; set; }
+
+        public string color_motif { get; set; }
+
+        public string event_date { get; set; }
+
+        public string church { get; set; }
+
+        public string venue { get; set; }
+
+        public string time_preparation { get; set; }
+
+        public string pax_number { get; set; }
+
+        public string additional_message { get; set; }
+    }
+}
