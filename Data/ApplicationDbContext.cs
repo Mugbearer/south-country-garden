e34494d8613c9ce0cdbe7ce7539da53990db0d6c
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-04 20:12:59 +0800

    db context update

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using south_country_garden.Model;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 
 namespace south_country_garden.Data
@@ -12,5 +13,11 @@ namespace south_country_garden.Data
         }
         
         public DbSet<booking_records> booking_records { get; set; }
+        public DbSet<booking_status> booking_status { get; set; }
+        public DbSet<event_add-ons> event_add_ons { get; set; }
+        public DbSet<event_details> event_details { get; set; }
+        public DbSet<event_inclusions> event_inclusions { get; set; }
+        public DbSet<food_arrangements> food_arrangements { get; set; }
+        public DbSet<terms_and_conditions> terms_and_conditions { get; set; }
     }
 }
