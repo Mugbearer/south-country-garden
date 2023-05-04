47b2f2132c686eb79283fd133f71adc2193bb0cb
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-04 18:14:17 +0800

    updated booking status model

--- Model/booking_status.cs
+++ Model/booking_status.cs
@@ -5,5 +5,14 @@ namespace south_country_garden.Model
     public class booking_status
     {
         public int booking_id { get; set; }
+
+        [MaxLength(255)]
+        public string remarks { get; set; }
+
+        [MaxLength(255)]
+        public string payment_status { get; set; }
+
+        [MaxLength(20)]
+        public string status { get; set; }
     }
 }
