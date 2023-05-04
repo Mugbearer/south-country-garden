d088d843d622590621b3adaa772540174d4a02f5
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-04 18:33:33 +0800

    event add-ons model updated

--- Model/event_add-ons.cs
+++ Model/event_add-ons.cs
@@ -5,5 +5,26 @@ namespace south_country_garden.Model
     public class event_add_ons
     {
         public int booking_id { get; set; }
+
+        [MaxLength(255)]
+        public string hair_and_make_up { get; set; }
+
+        [MaxLength(255)]
+        public string giveaway { get; set; }
+
+        [MaxLength(255)]
+        public string photo_and_video_coverage { get; set; }
+
+        [MaxLength(255)]
+        public string photo_booth { get; set; }
+
+        [MaxLength(255)]
+        public string bridal_car { get; set; }
+
+        [MaxLength(255)]
+        public string entourage_church_flower { get; set; }
+
+        [MaxLength(255)]
+        public string additional_stages { get; set; }
     }
 }
