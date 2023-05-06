dcffd59e80fc67e246af59cce61714f5f272d22e
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-06 17:16:06 +0800

    stuff

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace south_country_garden.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
