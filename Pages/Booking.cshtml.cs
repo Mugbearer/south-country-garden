8c94a7a7bb1cb5e7887a2108834273907cbd2b56
Hobey Padlan <hobeypadlanxx@gmail.com>
2023-05-20 19:56:51 +0800

    added email system

--- Pages/Booking.cshtml.cs
+++ Pages/Booking.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -39,7 +40,42 @@ namespace south_country_garden.Pages
             _context.booking_records.Add(booking_records);
             await _context.SaveChangesAsync();
 
+            SendEmail(booking_records);
+
             return RedirectToPage("./Index");
+        }
+
+        private static void SendEmail(booking_records data)
+        {
+            MailMessage clientMail = new MailMessage();
+            SmtpClient clientOne = new SmtpClient("smtp.office365.com");
+            clientMail.From = new MailAddress("kris.p.bacon2023@outlook.com", "Kris P. Bacon");
+            clientMail.To.Add(data.email);
+
+            clientMail.Subject = "Preliminary Quote";
+            clientMail.IsBodyHtml = true;
+            clientMail.Body = $"Greetings {data.name}!" +
+                $"<br><br>Thank you for booking South Country Garden. Please expect a response soon!";
+            clientOne.EnableSsl = true;
+            clientOne.Port = 587;
+            clientOne.Credentials = new System.Net.NetworkCredential("kris.p.bacon2023@outlook.com", "SamplePassword123");
+            clientOne.Send(clientMail);
+
+            MailMessage scgMail = new MailMessage();
+            SmtpClient clientTwo = new SmtpClient("smtp.office365.com");
+            scgMail.From = new MailAddress("kris.p.bacon2023@outlook.com", "Kris P. Bacon");
+            scgMail.To.Add("kris.p.bacon2023@outlook.com");
+
+            scgMail.Subject = "New Client Booking";
+            scgMail.IsBodyHtml = true;
+            scgMail.Body = "Iformation on new client booking:<br><br>" +
+                $"<b>Full Name:</b> {data.name}<br><b>Contact Number:</b> {data.contact_number}<br>" +
+                $"<b>Email Address:</b> {data.email}<br>" +
+                $"<b>Address:</b> {data.address}<br><b>Additional Message:</b> {data.additional_message}";
+            clientTwo.EnableSsl = true;
+            clientTwo.Port = 587;
+            clientTwo.Credentials = new System.Net.NetworkCredential("kris.p.bacon2023@outlook.com", "SamplePassword123");
+            clientTwo.Send(scgMail);
         }
     }
 }
